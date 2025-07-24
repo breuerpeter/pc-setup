02801678ef9060874166b995a05894de74cb5a93
Peter Breuer <peter.breuer.profiles@gmail.com>
2025-07-24 09:06:16 -0700

    fix: autostart apps

--- ubuntu/config/devilspie2/autostart_ff.lua
+++ ubuntu/config/devilspie2/autostart_ff.lua
@@ -1,24 +1,28 @@
-if get_window_name() == "Cursor" then
+debug_print("Window name: " .. get_window_name());
+debug_print("Application name: " .. get_application_name())
+
+if string.match(get_window_name(), "Cursor") then
     set_window_position(0, 0)      -- Left screen
     set_window_size(3840, 2160)    -- Full left screen size
 end
 
-if get_window_name() == "Vivaldi" then
+if string.match(get_window_name(), "Vivaldi") then
     set_window_position(3840, 0)   -- Right screen
     set_window_size(2160, 3840)    -- Full right screen size
 end
 
-if get_window_name() == "GitKraken" then
+if string.match(get_window_name(), "GitKraken") then
     set_window_position(3840, 0)   -- Right screen
     set_window_size(2160, 3840)    -- Full right screen size
 end
 
-if get_window_name() == "WezTerm" then
+-- Not working bc WezTerm uses Wayland not X11
+if string.match(get_window_name(), "WezTerm") then
     set_window_position(3840, 0)   -- Right screen
     set_window_size(2160, 3840)    -- Full right screen size
 end
 
-if get_window_name() == "Obsidian" then
+if string.match(get_window_name(), "Obsidian") then
     set_window_position(3840, 0)   -- Right screen
     set_window_size(2160, 3840)    -- Full right screen size
 end 
