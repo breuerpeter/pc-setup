2a52fcfa339f4d47b01e7c60869fa76b9c556190
Peter Breuer <peter.breuer.profiles@gmail.com>
2025-08-18 09:40:53 -0700

    fix: remove Wayland apps from devilspie2 config (don't work)

--- ubuntu/config/devilspie2/autostart_ff.lua
+++ ubuntu/config/devilspie2/autostart_ff.lua
@@ -6,18 +6,7 @@ if string.match(get_window_name(), "Cursor") then
     set_window_size(3840, 2160)    -- Full left screen size
 end
 
-if string.match(get_window_name(), "Chrome") then
-    set_window_position(3840, 0)   -- Right screen
-    set_window_size(2160, 3840)    -- Full right screen size
-end
-
 if string.match(get_window_name(), "GitKraken") then
-    set_window_position(3840, 0)   -- Right screen
-    set_window_size(2160, 3840)    -- Full right screen size
-end
-
--- Not working bc WezTerm uses Wayland not X11
-if string.match(get_window_name(), "WezTerm") then
     set_window_position(3840, 0)   -- Right screen
     set_window_size(2160, 3840)    -- Full right screen size
 end
@@ -26,3 +15,5 @@ if string.match(get_window_name(), "Obsidian") then
     set_window_position(3840, 0)   -- Right screen
     set_window_size(2160, 3840)    -- Full right screen size
 end 
+
+-- TODO: Wayland apps (WezTerm, Chrome)
